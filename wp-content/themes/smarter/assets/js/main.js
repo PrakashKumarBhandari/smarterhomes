ed8b1afedf0d8329e3a412f10581a0364d09071c
Prakash Bhandari <akash2046@gmail.com>
2021-08-23 00:12:47 +0545

    auto play and plugin

--- wp-content/themes/smarter/assets/js/main.js
+++ wp-content/themes/smarter/assets/js/main.js
@@ -59,12 +59,13 @@ const $rootNav = $('.cSlider--nav');
 
  $rootSingle.slick({
 	slide: '.cSlider__item',
+	autoplay:true,
  	slidesToShow: 1,
  	slidesToScroll: 1,
  	arrows: true,
  	fade: false,
  	adaptiveHeight: true,
- 	infinite: false,
+ 	infinite: true,
 	useTransform: true,
  	speed: 400,
  	cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
