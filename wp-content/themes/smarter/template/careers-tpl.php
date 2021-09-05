56ea91a1999f4a9aa9575c4d6eef681a4293cdd8
suzam shrestha <suzambstha@gmail.com>
2021-09-05 16:58:48 +0545

    animation on career

--- wp-content/themes/smarter/template/careers-tpl.php
+++ wp-content/themes/smarter/template/careers-tpl.php
@@ -8,11 +8,11 @@ get_header();?>
     <section class="section" id="section0" >
         <div class="title-banner halfbanner" style="background-image: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.5) 100%, rgba(0,212,255,1) 100%), url(<?php echo get_template_directory_uri();?>/assets/images/banner/heading/banner1.jpg">
             <div class="container-fluid">
-                <div class="heading-contents">
+                <div class="heading-contents wow fadeInDown">
                     <div class="heading-title">
                         <h1>Your Future Career in Company!</h1>
                     </div>
-                    <div class="heading-paragraph">
+                    <div class="heading-paragraph wow fadeInDown">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae consequatur possimus repellat mollitia illo alias nobis consectetur dolorum, iure animi error.</p>
                     </div>
                 </div>
@@ -23,14 +23,14 @@ get_header();?>
                 <div class="contents-about">
                     <div class="row">
                         <div class="col-md-4">
-                            <div class="section-title">
+                            <div class="section-title wow fadeInDown">
                                 <div class="main-title">
                                     <h2><span class="color-blk">Furtured</span><span class="mg-l color-grn font-weight-bold"> Careers</span></h2>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-8">
-                            <div class="cnt-abt">
+                            <div class="cnt-abt wow fadeInUp">
                                 Smarter Homes, LLC mission is to provide reliable, affordable renewable energy solutions to residential homes. Our focus is on providing hybrid and off-grid solar solutions to make homes less dependent on public utilities by being environmentally “green” friendly and help saving money and at the same time upgrading your home to the 21st century by overall smart solutions including video surveillance (“CCTV”) and even digitizing & protecting your home. <br><br>Since our competitors usually just focus on solar panels and maybe some sort of standalone battery systems, we provide a whole house solution from energy production (solar panels) to whole house ups (never have to reset your clock anymore in case of power outage..) to energy optimization (smart energy management inside the house from light switches to outlets to HVAC energy efficient & smart solutions – and more). All combined with central easy to manage high-end (but easy to use) home automation and management devices.
                             </div>
                         </div>
@@ -43,14 +43,14 @@ get_header();?>
 
     <section class="section " id="section1">
         <div class="container-fluid">
-            <div class="section-title">
+            <div class="section-title wow fadeInUp">
                 <div class="main-title text-center">
                     <h2><span class="mg-l">Meet Our</span><span class="mg-l color-grn font-weight-bold"> Professionals</span></h2>
                 </div>
                 <div class="section-paragraph mx-w830 text-center margin-center mt-20">Solar is not just a phase – it’s an aggressively growing alternative as more and more people realize that they can save money every month by using solar panels.</div>
             </div>
             <div class="tabs-contents">
-                <ul class="nav nav-pills mb-3 nav-justified" id="pills-tab" role="tablist">
+                <ul class="nav nav-pills mb-3 nav-justified wow fadeInUp" id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
                         <a class="nav-link active" id="pills-manager-tab" data-toggle="pill" href="#pills-manager" role="tab" aria-controls="pills-managerr" aria-selected="true">Manager</a>
                     </li>
@@ -65,13 +65,13 @@ get_header();?>
                     <div class="tab-pane fade show active" id="pills-manager" role="tabpanel" aria-labelledby="pills-manager-tab">
                         <div class="tabs-cnts">
                             <div class="row">
-                                <div class="col-md-5">
+                                <div class="col-md-5 wow fadeInLeft">
                                     <div class="img-tabsContents">
                                         <img src="<?php echo get_template_directory_uri();?>/assets/images/others/image1.JPG" alt="">
                                     </div>
                                 </div>
 
-                                <div class="col-md-7">
+                                <div class="col-md-7 wow fadeInRight">
                                     <div class="paragraph-tabsContents">
                                         <h3>Luke Bytalker</h3>
                                         <h5>Manager</h5>
@@ -136,43 +136,208 @@ get_header();?>
 
     <section class="section fullcoverPage" id="section2" style="background-image: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.5) 100%, rgba(0,212,255,1) 100%), url(<?php echo get_template_directory_uri();?>/assets/images/others/image2.jpg">
         <div class="container-fluid">
-            <div class="section-title">
-                <div class="main-title">
-                    <h2><span class="color-ylw font-weight-bold">Responsibility</span></h2>
-                    <div class="section-paragraph mx-w830 color-wht mt-20">Solar is not just a phase – it’s an aggressively growing alternative as more and more people realize that they can save money every month by using solar panels.</div>
+            <div class="row">
+                <div class="col-md-7 wow fadeInLeft">
+                    <div class="section-title">
+                        <div class="main-title">
+                            <h2><span class="color-ylw font-weight-bold">Responsibility</span></h2>
+                            <div class="section-paragraph mx-w830 color-wht mt-20">Solar is not just a phase – it’s an aggressively growing alternative as more and more people realize that they can save money every month by using solar panels.</div>
+                        </div>
+                    </div>
+                    <div class="responsibility-contents">
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3>Set priorities and build the infratructure.</h3>
+                                <p class="color-wht">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3>Estavlish feedback loops.</h3>
+                                <p class="color-wht">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3>Boot Cummunication and change management.</h3>
+                                <p class="color-wht">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+
                 </div>
             </div>
-            <div class="responsibility-contents">
-                <div class="respon_contentList">
-                    <div class="respon_contentTitle">
-                        <h3>Set priorities and build the infratructure.</h3>
-                        <p class="color-wht">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
-                    </div>
-                </div>
+        </div>
+    </section>
 
-                <div class="respon_contentList">
-                    <div class="respon_contentTitle">
-                        <h3>Estavlish feedback loops.</h3>
-                        <p class="color-wht">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
-                    </div>
+    <section class="section fullcoverPage" id="section3" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/others/image3.jpg">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-5">
+
                 </div>
-                <div class="respon_contentList">
-                    <div class="respon_contentTitle">
-                        <h3>Boot Cummunication and change management.</h3>
-                        <p class="color-wht">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                <div class="col-md-7  wow fadeInRight">
+                    <div class="section-title">
+                        <div class="main-title">
+                            <h2><span class="primary-color font-weight-bold">Responsibility</span></h2>
+                            <div class="section-paragraph mx-w830 mt-20">Solar is not just a phase – it’s an aggressively growing alternative as more and more people realize that they can save money every month by using solar panels.</div>
+                        </div>
+                    </div>
+                    <div class="responsibility-contents">
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3 class="color-black">Set priorities and build the infratructure.</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle ">
+                                <h3 class="color-black">Estavlish feedback loops.</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3 class="color-black">Boot Cummunication and change management.</h3>
+                                <p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="section" id="section3">
-      
+    <section class="section fullcoverPage" id="section4" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/others/image4.jpg">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-7  wow fadeInLeft">
+                    <div class="section-title">
+                        <div class="main-title">
+                            <h2><span class="primary-color font-weight-bold">Application</span></h2>
+                            <div class="section-paragraph mx-w830 mt-20">Solar is not just a phase – it’s an aggressively growing alternative as more and more people realize that they can save money every month by using solar panels.</div>
+                        </div>
+                    </div>
+                    <div class="responsibility-contents">
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3 class="color-black">Set priorities and build the infratructure.</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle ">
+                                <h3 class="color-black">Estavlish feedback loops.</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+                        <div class="respon_contentList">
+                            <div class="respon_contentTitle">
+                                <h3 class="color-black">Boot Cummunication and change management.</h3>
+                                <p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ratione deserunt sunt laborum voluptatem voluptatum incidunt magnam, alias illum molestias similique doloremque vel architecto dignissimos eius mollitia excepturi asperiores eligendi?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+
+                </div>
+            </div>
+        </div>
     </section>
 
-    <section class="section" id="section4">
-      
-    </section>
+    <?php
+    $footer_bg_image = get_template_directory_uri().'/assets/images/banner/footer-banner.jpg';
+    $footer_block_image = get_field( 'footer_bg_image' );
+    if ( ! empty( $footer_block_image ) ) {
+    $footer_bg_image = $footer_block_image['url'];
+    }
+    ?>                        
+
+      <!-- Footer -->
+     <div class="section z-index-1 footer-fullpage fullcoverPage" id="section6" style="background-image: url(<?php echo $footer_bg_image;?>">
+         <div class="footer-form">
+             <div class="container-fluid">             
+             <?php echo do_shortcode('[contact-form-7 id="142" title="Contact form"]'); ?>
+             </div>
+         </div>
+ 
+         <div class="div contact-footer">
+             <div class="container-fluid">
+                 <div class="row">
+                     <div class="col-md-3">
+                         <div class="img-footer">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/logo-footer.png" alt="">
+                         </div>
+                     </div>
+                     <div class="col-md-9">
+                         <div class="cnt-footer">
+                             <div class="row">
+                                 <div class="col-md-3 col-6">
+                                     <div class="smarter-location">
+                                         Our Location <br>
+                                         <?php the_field( 'our_location' ); ?>
+                                     </div>
+                                 </div>
+                                 <div class="col-md-3 col-6">
+                                     <div class="smarter-location">
+                                         Phone: <br>
+                                         <?php the_field( 'phone' ); ?>
+                                     </div>
+                                 </div>
+                                 <div class="col-md-3 col-6">
+                                     <div class="smarter-location">
+                                         Email Address: <br>
+                                         <?php the_field( 'email_address' ); ?>
+                                     </div>
+                                 </div>
+                                 <div class="col-md-3 col-6">
+                                     <div class="smarter-location">
+                                        <?php the_field( 'timing_heading' ); ?> <br>
+                                        <?php the_field( 'timing_detail' ); ?>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+ 
+         <div class="btm-footer">
+             <div class="container-fluid">
+                 <div class="row">
+                     <div class="col-md-5 order-md-first order-last">
+                         <div class="copyright">© Copyright 2021   |   All Rights Reserved</div>
+                     </div>
+                     <div class="col-md-7 ">
+                         <div class="footer-menu">
+                            <?php 
+                            wp_nav_menu(
+                                array(
+                                'menu'      => 'Footer-Menu',
+                                'container'      => 'ul',
+                                )
+                            );
+                            ?>
+                             <!-- <ul>
+                                 <li><a href="">Home</a></li>
+                                 <li><a href="">About Us</a></li>
+                                 <li><a href="">Services</a></li>
+                                 <li><a href="">Careers</a></li>
+                                 <li><a href="">Contact Us</a></li>
+                             </ul> -->
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
 </div>
 
 
